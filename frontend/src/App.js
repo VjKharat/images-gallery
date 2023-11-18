81c32b399f8c816bb6cd581c1c278fb7502a6ac3
Vijay Kharat <vijaykharat1610@gmail.com>
2023-11-18 15:55:33 +0530

    fixed code with linter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,31 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-const UNPLASH_KEY=process.env.REACT_APP_UNPLASH_KEY;
+const UNPLASH_KEY = process.env.REACT_APP_UNPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
 
-  const handleSearchSubmit= (e) => {
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNPLASH_KEY}`)
-      .then((res)=> res.json())
+    fetch(
+      `https://api.unsplash.com/photos/random?query=${word}&client_id=${UNPLASH_KEY}`,
+    )
+      .then((res) => res.json())
       .then((data) => {
-        console.log(data);        
+        console.log(data);
       })
       .catch((err) => {
         console.log(err);
-      })
-      setWord('');
-  }
- 
-    return (
+      });
+    setWord('');
+  };
+
+  return (
     <div>
-     <Header title="Images Gallery"/>
-     <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import React from 'react';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 
-const Search = ({word, setWord, handleSubmit}) => {
+const Search = ({ word, setWord, handleSubmit }) => {
   return (
     <Container className="mt-3">
       <Row className="justify-content-center">
@@ -9,15 +9,17 @@ const Search = ({word, setWord, handleSubmit}) => {
           <Form onSubmit={handleSubmit}>
             <Form.Row>
               <Col xs={9}>
-                <Form.Control 
-                type="text"
-                value={word}
-                onChange={(e) => setWord(e.target.value)}
-                placeholder="Search for new images..." 
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for new images..."
                 />
               </Col>
               <Col>
-                <Button variant="primary" type="submit">Search</Button>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
               </Col>
             </Form.Row>
           </Form>
